2c0026b4f66890ad68f2815102091b6fb5788e2d
Yashwant937363 <yashwantpoyrekar@gmail.com>
2024-02-23 23:47:54 +0530

    Going Development After fix error

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
-import './Home.css';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import ChatWindow from './ChatWindow/ChatWindow';
-import { socket } from '../../store/socket';
-import SideBar from './Sidebar/SideBar';
+import React, { useEffect } from "react";
+import "./Home.css";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import ChatWindow from "./ChatWindow/ChatWindow";
+import { socket } from "../../store/socket";
+import SideBar from "./Sidebar/SideBar";
 
 export default function Home() {
   const isLogin = useSelector((state) => state.user.isLogin);
-  const openedchat = useSelector((state) => state.UIState.openedchat)
+  const openedchat = useSelector((state) => state.UIState.openedchat);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!isLogin) {
-      navigate('/login');
+      navigate("/login");
     }
   }, [isLogin, navigate]);
 
@@ -22,14 +22,17 @@ export default function Home() {
   }
 
   useEffect(() => {
-    socket.connect()
-  }, [])
+    socket.connect();
+  }, []);
 
   return (
-    <div className='home'>
-      <SideBar ></SideBar>
-      {openedchat ? <ChatWindow />
-        : <div className='closedchat'>Click on Chat to Open The Chat</div>}
+    <div className="home">
+      <SideBar></SideBar>
+      {openedchat ? (
+        <ChatWindow />
+      ) : (
+        <div className="closedchat">Click on Chat to Open The Chat</div>
+      )}
     </div>
   );
 }
